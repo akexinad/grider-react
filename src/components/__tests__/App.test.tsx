ffda711dc818ccaf7d8c2d151c0bc2e366089428
danny <danixeka@gmail.com>
2020-03-19 19:37:00 +1100

    27. Code Reuse with BeforeEach

--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, ShallowWrapper } from "enzyme";
 
 import App from "../App";
 import CommentBox from "../CommentBox";
@@ -23,9 +23,7 @@ import CommentList from "../CommentList";
  */
 
 describe("app component", () => {
-    const component = <App />;
-    const wrapper = shallow(component);
-
+    
     /**
      * we are actually creating a div inside of the browser,
      * the JSDOM library mocks up a browser like environment that
@@ -34,21 +32,30 @@ describe("app component", () => {
      * So this div is a mocked div that existts in the terminal,
      * not the browser.
      */
-
+    
     // const div = document.createElement("div");
+    
+    
+    let wrapped: ShallowWrapper;
+    
+    beforeEach(() => {
+        const component = <App />;
+        wrapped = shallow(component);    
+    })
 
     afterEach(() => {
         // this cleans up anything stored in memory to speed up tests.
+        
         // ReactDOM.unmountComponentAtNode(div);
     });
 
     it("shows a comment box", () => {
 
-        expect(wrapper.find(CommentBox).length).toBe(1);
+        expect(wrapped.find(CommentBox).length).toBe(1);
         // expect(wrapper.find(CommentBox).length).toBe(2);
     });
 
     it("shows a comment list", () => {
-        expect(wrapper.find(CommentList).length).toBe(1);
+        expect(wrapped.find(CommentList).length).toBe(1);
     })
 });
