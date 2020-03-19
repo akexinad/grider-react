3efe9e7f8c505a28336d61e15e20a2f166e4d91d
danny <danixeka@gmail.com>
2020-03-19 18:55:36 +1100

    16. Tricking React with JSDOM

--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -3,3 +3,29 @@ import ReactDOM from "react-dom";
 
 import App from "../App";
 
+describe("app component", () => {
+
+    const component = <App />;
+    
+    /**
+     * we are actually creating a div inside of the browser,
+     * the JSDOM library mocks up a browser like environment that
+     * jest understands.
+     *
+     * So this div is a mocked div that existts in the terminal,
+     * not the browser.
+     */
+
+    const div = document.createElement("div");
+
+    afterAll(() => {
+        // this cleans up anything stored in memory to speed up tests.
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it("shows a comment box", () => {
+        ReactDOM.render(component, div);
+    });
+
+    
+});
