e4bcd80d8f210d5a9edc6ab9813b2951cf220f3f
danny <danixeka@gmail.com>
2020-03-19 19:26:36 +1100

    22. Expectations for Component Instances

--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -5,20 +5,20 @@ import App from "../App";
 import CommentBox from "../CommentBox";
 
 /**
- * 
+ *
  * ENZYME
- * 
+ *
  * We have three rendering options with enzyme
- * 
+ *
  * STATIC
  * - Render the given component and return plain HTML
- * 
+ *
  * SHALLOW
  * - Render just the given component and none of its children.
- * 
+ *
  * FULL DOM
  * - Render the component and all of its children and let us modify it afterwards.
- * 
+ *
  */
 
 describe("app component", () => {
@@ -33,18 +33,17 @@ describe("app component", () => {
      * not the browser.
      */
 
-    const div = document.createElement("div");
+    // const div = document.createElement("div");
 
     afterEach(() => {
         // this cleans up anything stored in memory to speed up tests.
-        ReactDOM.unmountComponentAtNode(div);
+        // ReactDOM.unmountComponentAtNode(div);
     });
 
     it("shows a comment box", () => {
-
         const wrapper = shallow(component);
 
-        wrapper.find(CommentBox)
-        
+        expect(wrapper.find(CommentBox).length).toBe(1);
+        // expect(wrapper.find(CommentBox).length).toBe(2);
     });
 });
