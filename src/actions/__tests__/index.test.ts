2b227ff4c601c94cf458e191f4ea0995c2ab32cf
danny <danixeka@gmail.com>
2020-03-20 14:08:45 +1100

    49. Testing Action Creators

---
+++ src/actions/__tests__/index.test.ts
@@ -0,0 +1,15 @@
+import { SAVE_COMMENT } from "../types";
+import { saveComment } from "..";
+
+describe("saveComment", () => {
+    const newComment = "this is a new comment";
+    const action = saveComment(newComment);
+
+    it("has the correct type", () => {
+        expect(action.type).toEqual(SAVE_COMMENT);
+    });
+
+    it("has the correct payload", () => {
+        expect(action.payload).toEqual(newComment);
+    });
+});
