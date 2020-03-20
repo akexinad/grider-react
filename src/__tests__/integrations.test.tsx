38d0448219a3da10863c95090f3e531c20acfa16
danny <danixeka@gmail.com>
2020-03-20 18:34:55 +1100

    58. Integration Tests in Action

---
+++ src/__tests__/integrations.test.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { mount } from "enzyme";
+
+import Root from "../Root";
+import App from "../components/App";
+
+afterEach(() => {
+
+    
+    
+})
+
+it("can fetch a list of comments and display them", () => {
+
+    // Attempt to render the entire app
+
+    
+    
+});
