764a4bda6feb18216b27cef730eb569318e4c329
danny <danixeka@gmail.com>
2020-03-20 18:31:32 +1100

    prettified

--- src/reducers/comments.ts
+++ src/reducers/comments.ts
@@ -1,12 +1,13 @@
 import { IComment, ICommentActionTypes } from "../actions/types";
-import { AxiosResponse } from "axios";
 
 export default (state: Array<IComment> = [], action: ICommentActionTypes) => {
     switch (action.type) {
         case "SAVE_COMMENT":
             return [...state, action.payload];
         case "FETCH_COMMENTS":
-            const comments = action.payload.data.map((comment: any) => comment.name);
+            const comments = action.payload.data.map(
+                (comment: any) => comment.name
+            );
 
             return [...state, ...comments];
         default:
