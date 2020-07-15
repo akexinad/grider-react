39a4db3eadbb2729a59a50447f366cfc0eb83039
danny <danixeka@gmail.com>
2020-07-15 15:49:42 +1000

    completed tests for comment box

--- src/components/__tests__/CommentBox.test.tsx
+++ src/components/__tests__/CommentBox.test.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import CommentBox from "components/CommentBox";
 import { ReactWrapper, mount, ShallowWrapper, shallow } from "enzyme";
 
-describe("the app component", () => {
+describe("the component layout", () => {
     /**
      * we can get away with the shallow renderer here as we do not need
      * access to the components chidren.
      */
     // let wrapped: ReactWrapper;
     let wrapped: ShallowWrapper;
-
+    
     type testElements = Array<[string, number]>;
     const TEXT_AREA = "textarea";
     const BUTTON = "button";
@@ -17,7 +17,7 @@ describe("the app component", () => {
         [TEXT_AREA, 1],
         [BUTTON, 1]
     ];
-
+    
     beforeEach(() => {
         // wrapped = mount(<CommentBox />);
         wrapped = shallow(<CommentBox />);
@@ -31,33 +31,31 @@ describe("the app component", () => {
         expect(wrapped.find(element).length).toEqual(qty);
     });
 
-    it("has a text area that users can type in", () => {
-        const changeEvent = "change";
-        const mockEventObject = {
-            target: {
-                value: "my name is bla bla"
-            }
-        };
-        const valueProp = "value";
+    const changeEvent = "change";
+    const submitEvent = "submit";
+    const mockEventObject = {
+        target: {
+            value: "bla bla bla"
+        }
+    };
 
+    it("has a text area that users can type in", () => {
         // SIMULATE A CHANGE EVENT WITH A MOCK OBJECT.
         wrapped.find(TEXT_AREA).simulate(changeEvent, mockEventObject);
 
         // MAKE THE COMPONENT RERENDER.
         wrapped.update();
-
         // now we can check the "value" prop belonging to the textarea to see if it equals the value of the mock object above.
-        expect(wrapped.find(TEXT_AREA).prop(valueProp)).toEqual(mockEventObject.target.value);
+        expect(wrapped.find(TEXT_AREA).prop("value")).toEqual(
+            mockEventObject.target.value
+        );
     });
 
-    it("should empty out the text area when the form is submitted", () => {
-        const submitEvent = "submit";
-        const valueProp = "value";
-
+    it("should be empty out the text area when the form is submitted", () => {
         wrapped.find(TEXT_AREA).simulate(submitEvent);
 
         wrapped.update();
 
-        expect(wrapped.find(TEXT_AREA).prop(valueProp)).toEqual("");
-    })
+        expect(wrapped.find(TEXT_AREA).prop("value")).toEqual("");
+    });
 });
